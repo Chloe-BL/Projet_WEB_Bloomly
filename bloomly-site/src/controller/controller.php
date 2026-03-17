53d8741b556e2ba24efdf4d80159d1091b7dfb4d
ambre <ambre.boulbair@viacesi.fr.org>
2026-03-17 11:39:12 +0100

    page appreciation ca maaaaaaaaarche

--- bloomly-site/src/controller/controller.php
+++ bloomly-site/src/controller/controller.php
@@ -19,31 +19,40 @@ class TestController
     $message = "";
 
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $action = $_POST['action'] ?? '';
+
+        if ($action == 'etud'){
+            $lettre = htmlspecialchars($_POST['Lettre'] ?? '', ENT_QUOTES, 'UTF-8');
+
+            $upload = new Upload();
+            $validation = $upload -> validerFichier($_FILES['cv']);
+
+            if ($validation === true) {
+                $nomFichier = $upload->enregistrerFichier($_FILES['cv']);
+
+                if ($nomFichier) {
+                    $message = "Candidature envoyée avec succès.<br>";
+                    $message .= "CV téléversé : " . htmlspecialchars($nomFichier, ENT_QUOTES, 'UTF-8') . "<br>";
+                    $message .= "Lettre de motivation : " . nl2br($lettre);
+                } else {
+                    $message = "Erreur lors de l'enregistrement du fichier.";
+                }
+            } else {
+                $message = $validation;
+            }
+        }
+
+        elseif ($action == 'admin_pil'){
         $lettre = htmlspecialchars($_POST['Lettre'] ?? '', ENT_QUOTES, 'UTF-8');
 
-        $upload = new Upload();
-        $validation = $upload -> validerFichier($_FILES['cv']);
-
-        if ($validation === true) {
-            $nomFichier = $upload->enregistrerFichier($_FILES['cv']);
-
-            if ($nomFichier) {
-                $message = "Candidature envoyée avec succès.<br>";
-                $message .= "CV téléversé : " . htmlspecialchars($nomFichier, ENT_QUOTES, 'UTF-8') . "<br>";
-                $message .= "Lettre de motivation : " . nl2br($lettre);
-            } else {
-                $message = "Erreur lors de l'enregistrement du fichier.";
-            }
-        } else {
-            $message = $validation;
+        $message .= "Appréciation : " . nl2br($lettre);
         }
     }
-
-    echo $this -> twig -> render('candidature.twig', // render sert à afficher le template Twig
+            echo $this -> twig -> render('candidature.twig', // render sert à afficher le template Twig
     // C'est un tableau associatif qui permet de passer des variables au template Twig
-    [ 
+        [ 
         'message' => $message // On utilisera : {{ message }}
-    ]);
+        ]);
 }
 
     public function liste( array $items, $section){
@@ -227,7 +236,7 @@ class TestController
     }
 
     public function mon_espace(){
-        $user = $this -> getUser();
+        $user = $_GET['user'];
         $civility = 'Madame';
         $nom = 'Dupont';
         $prenom = 'Jean';
