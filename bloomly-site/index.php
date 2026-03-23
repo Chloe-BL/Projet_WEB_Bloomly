737ec7cedee1ba651837e75d42bdbacd441c2922
ambre <ambre.boulbair@viacesi.fr.org>
2026-03-23 10:50:49 +0100

    pret pour test final

--- bloomly-site/index.php
+++ bloomly-site/index.php
@@ -4,8 +4,19 @@
 error_reporting(E_ALL); 
 ini_set('display_errors', 1);
 
+
 require_once __DIR__ . '/vendor/autoload.php'; // Chargement de l'autoload de Composer pour Twig et autres dépendances
-require_once __DIR__ . '/src/controller/controller.php'; // Chargement du contrôleur
+require_once __DIR__ . '/src/Controller/BaseController.php';
+require_once __DIR__ . '/src/Controller/PageController.php';
+require_once __DIR__ . '/src/Controller/AuthController.php';
+require_once __DIR__ . '/src/Controller/SectionController.php';
+require_once __DIR__ . '/src/Controller/CandidatureController.php';
+require_once __DIR__ . '/src/Model/UserModel.php';
+require_once __DIR__ . '/src/Model/SectionModel.php';
+require_once __DIR__ . '/src/Model/ProfileModel.php';
+require_once __DIR__ . '/src/Outils/Pagination.php';
+require_once __DIR__ . '/src/Outils/Upload.php';
+ 
 
 $page = $_GET['page'] ?? '/bloomly-site.local/index';
 
