1f6ee2c3910a7fec7385a1bbf5f86f401d4e5993
ambre <ambre.boulbair@viacesi.fr.org>
2026-03-23 16:35:40 +0100

    test de connexion bdd

--- bloomly-site/test_BDD.php
+++ bloomly-site/test_BDD.php
@@ -1,21 +1,11 @@
 <?php
+ 
+require_once 'config/dataBase.php';
 
-$host = 'localhost';
-$port = '3306';
-$dbname = 'Bloomly';
-$user = 'Bloomly_user';
-$password = 'Bloomly2026!';
-
-try {
-    $pdo = new PDO(
-        "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4",
-        $user,
-        $password
-    );
-
-    $pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    echo "Connexion réussie";
-} catch (PDOException $e) {
-    echo "Erreur : " . $e -> getMessage();
-}
+$sql = "SELECT * FROM entreprise";
+$stmt = $pdo->query($sql);
+$entreprises = $stmt->fetchAll();
+echo "<pre>";
+print_r($entreprises);
+echo "</pre>";
+ 
