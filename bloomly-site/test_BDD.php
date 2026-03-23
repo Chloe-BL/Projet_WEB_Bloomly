62af0fdc02c9a8cfca7b0a8d1b26f5a867cd7d83
Chloé <chloe.blanc@viacesi.fr>
2026-03-23 16:27:00 +0100

    test BDD

---
+++ bloomly-site/test_BDD.php
@@ -0,0 +1,21 @@
+<?php
+
+$host = 'localhost';
+$port = '3306';
+$dbname = 'Bloomly';
+$user = 'Bloomly_user';
+$password = 'Bloomly2026!';
+
+try {
+    $pdo = new PDO(
+        "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4",
+        $user,
+        $password
+    );
+
+    $pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    echo "Connexion réussie";
+} catch (PDOException $e) {
+    echo "Erreur : " . $e->getMessage();
+}
