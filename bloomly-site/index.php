ddb880197e7f7adf0a0745a15b64e794106f2169
ambre <ambre.boulbair@viacesi.fr.org>
2026-03-23 10:52:06 +0100

    enleve les maj

--- bloomly-site/index.php
+++ bloomly-site/index.php
@@ -6,16 +6,16 @@ ini_set('display_errors', 1);
 
 
 require_once __DIR__ . '/vendor/autoload.php'; // Chargement de l'autoload de Composer pour Twig et autres dépendances
-require_once __DIR__ . '/src/Controller/BaseController.php';
-require_once __DIR__ . '/src/Controller/PageController.php';
-require_once __DIR__ . '/src/Controller/AuthController.php';
-require_once __DIR__ . '/src/Controller/SectionController.php';
-require_once __DIR__ . '/src/Controller/CandidatureController.php';
-require_once __DIR__ . '/src/Model/UserModel.php';
-require_once __DIR__ . '/src/Model/SectionModel.php';
-require_once __DIR__ . '/src/Model/ProfileModel.php';
-require_once __DIR__ . '/src/Outils/Pagination.php';
-require_once __DIR__ . '/src/Outils/Upload.php';
+require_once __DIR__ . '/src/controller/BaseController.php';
+require_once __DIR__ . '/src/controller/PageController.php';
+require_once __DIR__ . '/src/controller/AuthController.php';
+require_once __DIR__ . '/src/controller/SectionController.php';
+require_once __DIR__ . '/src/controller/CandidatureController.php';
+require_once __DIR__ . '/src/model/UserModel.php';
+require_once __DIR__ . '/src/model/SectionModel.php';
+require_once __DIR__ . '/src/model/ProfileModel.php';
+require_once __DIR__ . '/src/outils/Pagination.php';
+require_once __DIR__ . '/src/outils/Upload.php';
  
 
 $page = $_GET['page'] ?? '/bloomly-site.local/index';
