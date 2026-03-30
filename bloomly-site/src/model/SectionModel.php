9ec14d40c0fc22bc322f28632117abb0c8f9274b
Chloé <chloe.blanc@viacesi.fr>
2026-03-30 16:51:00 +0200

    Réparation des boutons et des sql etudiants

--- bloomly-site/src/model/SectionModel.php
+++ bloomly-site/src/model/SectionModel.php
@@ -6,38 +6,25 @@ class SectionModel extends BaseModel
 {
     public function getItemsBySection(string $section) {
  
- 
-        $pilots = [
-            ["nom" => "Martin", "prenom" => "Lucas"],
-            ["nom" => "Nguyen", "prenom" => "Linh"],
-            ["nom" => "Diallo", "prenom" => "Aminata"],
-            ["nom" => "Garcia", "prenom" => "Carlos"],
-            ["nom" => "Kowalski", "prenom" => "Anna"],
-            ["nom" => "Benali", "prenom" => "Yassine"],
-            ["nom" => "Dubois", "prenom" => "Emma"],
-            ["nom" => "Santos", "prenom" => "Mateus"],
-            ["nom" => "Kim", "prenom" => "Jisoo"],
-            ["nom" => "Rossi", "prenom" => "Giulia"],
-            ["nom" => "Haddad", "prenom" => "Nour"],
-            ["nom" => "Moreau", "prenom" => "Gabriel"],
-            ["nom" => "Singh", "prenom" => "Arjun"],
-            ["nom" => "Ivanov", "prenom" => "Dmitri"],
-            ["nom" => "Fernandez", "prenom" => "Sofia"],
-            ["nom" => "Traoré", "prenom" => "Moussa"],
-            ["nom" => "Schmidt", "prenom" => "Lena"],
-            ["nom" => "Alvarez", "prenom" => "Diego"],
-            ["nom" => "Okafor", "prenom" => "Chinedu"],
-            ["nom" => "Tanaka", "prenom" => "Yuki"]
-        ];
- 
         if ($section === 'offres') {
             $user_actif = $_COOKIE['user_id'] ?? null;
-            $sql = "SELECT id, titre FROM offres WHERE id_createur = ?";
-            $stmt = $this->pdo->prepare($sql);
-            $stmt->execute([$user_actif]);
-            $offres = $stmt -> fetchAll();
+            $user = $_GET['user'] ?? null;
+
+            if ($user === '3') {
+                $sql = "SELECT id, titre FROM offres";
+                $stmt = $this->pdo->query($sql);
+                $offres = $stmt -> fetchAll();
+            }
+
+            else {
+                $sql = "SELECT id, titre FROM offres WHERE id_createur = ?";
+                $stmt = $this->pdo->prepare($sql);
+                $stmt->execute([$user_actif]);
+                $offres = $stmt -> fetchAll();
+            }
             return $offres;
         }
+    
  
         if ($section === 'entreprises') {
             $user_actif = $_COOKIE['user_id'] ?? null;
